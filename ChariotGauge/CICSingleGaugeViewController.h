8bd1e3e73f5fa6f02b32d0560dc023450c488eee
mikemey01 <mikemey01@gmail.com>
2013-12-19 21:37:27 -0700

    setup single gauge view controller

---
+++ ChariotGauge/CICSingleGaugeViewController.h
@@ -0,0 +1,21 @@
+//
+//  CICSingleGaugeViewController.h
+//  ChariotGauge
+//
+//  Created by Mike on 12/19/13.
+//  Copyright (c) 2013 Chariot Instruments. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CICGaugeBuilder.h"
+
+@class CICGaugeBuilder;
+
+@interface CICSingleGaugeViewController : UIViewController{
+    CICGaugeBuilder *gaugeView;
+}
+
+@property (nonatomic, retain) IBOutlet CICGaugeBuilder *gaugeView;
+
+
+@end
---
+++ ChariotGauge/CICSingleGaugeViewController.m
@@ -0,0 +1,60 @@
+//
+//  CICSingleGaugeViewController.m
+//  ChariotGauge
+//
+//  Created by Mike on 12/19/13.
+//  Copyright (c) 2013 Chariot Instruments. All rights reserved.
+//
+
+
+#import "CICSingleGaugeViewController.h"
+#import "CICGaugeBuilder.h"
+#import "CICAppDelegate.h"
+
+
+@interface CICSingleGaugeViewController ()
+
+@end
+
+@implementation CICSingleGaugeViewController
+
+@synthesize gaugeView;
+
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+//{
+//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+//    if (self) {
+//        // Custom initialization
+//    }
+//    return self;
+//}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.gaugeView initializeGauge]; //NECESSARY
+    self.gaugeView.minGaugeNumber = -30;
+    self.gaugeView.maxGaugeNumber = 25;
+    self.gaugeView.gaugeLabel = @"Boost/Vac";
+    self.gaugeView.incrementPerLargeTick = 5;
+    self.gaugeView.tickStartAngleDegrees = 135;
+    self.gaugeView.tickDistance = 270;
+    self.gaugeView.menuItemsFont = [UIFont fontWithName:@"Helvetica" size:14];
+    self.gaugeView.lineWidth = 1;
+    self.gaugeView.value = self.gaugeView.minGaugeNumber;
+    
+}
+
+- (void)viewDidUnload
+{
+    self.gaugeView = nil;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
