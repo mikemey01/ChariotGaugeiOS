13a26d1856fb1015fe314a0e56059b6e22ab73ca
mikemey01 <mikemey01@gmail.com>
2013-12-22 09:47:04 -0700

    added home screen view controller

---
+++ CICHomeScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  CICHomeScreenViewController.h
+//  ChariotGauge
+//
+//  Created by Mike on 12/22/13.
+//  Copyright (c) 2013 Chariot Instruments. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CICHomeScreenViewController : UIViewController
+
+@end
---
+++ CICHomeScreenViewController.m
@@ -0,0 +1,38 @@
+//
+//  CICHomeScreenViewController.m
+//  ChariotGauge
+//
+//  Created by Mike on 12/22/13.
+//  Copyright (c) 2013 Chariot Instruments. All rights reserved.
+//
+
+#import "CICHomeScreenViewController.h"
+
+@interface CICHomeScreenViewController ()
+
+@end
+
+@implementation CICHomeScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
