d935631eead3397e4e892eda0eb549c150f55cc8
mikemey01 <mikemey01@gmail.com>
2013-12-28 17:24:54 -0700

    Added faded line to home screen

---
+++ ChariotGauge/CICDrawLine.h
@@ -0,0 +1,13 @@
+//
+//  CICDrawLine.h
+//  ChariotGauge
+//
+//  Created by Mike on 12/28/13.
+//  Copyright (c) 2013 Chariot Instruments. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CICDrawLine : UIView
+
+@end
---
+++ ChariotGauge/CICDrawLine.m
@@ -0,0 +1,53 @@
+//
+//  CICDrawLine.m
+//  ChariotGauge
+//
+//  Created by Mike on 12/28/13.
+//  Copyright (c) 2013 Chariot Instruments. All rights reserved.
+//
+
+#import "CICDrawLine.h"
+
+#define   DIAMETER  self.frame.size.width
+
+@implementation CICDrawLine
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    [super drawRect:rect];
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+//    
+//    CGContextSaveGState(context);
+//    
+//    [[UIColor lightGrayColor] setFill];
+//    UIRectFill((CGRect){0,10,rect.size.width,1});
+//    
+//    CGContextRestoreGState(context);
+    
+    CGFloat colors[16] = { 0,0, 0, 0,
+        0, 0, 0, .5,
+        0, 0, 0, .5,
+        0, 0,0 ,0 };
+    CGColorSpaceRef baseSpace = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(baseSpace, colors, NULL, 4);
+    
+    CGContextSaveGState(context);
+    CGContextAddRect(context, CGRectMake(CGRectGetMinX(rect),CGRectGetMinY(rect), rect.size.width, 1));
+    CGContextClip(context);
+    CGContextDrawLinearGradient (context, gradient, CGPointMake(CGRectGetMinX(rect), CGRectGetMinY(rect)), CGPointMake(CGRectGetMaxX(rect),CGRectGetMaxY(rect)), 0);
+    CGContextRestoreGState(context);
+    
+}
+
+
+@end
