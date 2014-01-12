bc7ac9627e1245db05cb31a22a63bf88f1e682d2
mikemey01 <mikemey01@gmail.com>
2014-01-12 15:04:26 -0700

    adding bluetooth handler object

---
+++ ChariotGauge/CICBluetoothHandler.h
@@ -0,0 +1,13 @@
+//
+//  CICBluetoothHandler.h
+//  ChariotGauge
+//
+//  Created by Mike on 1/12/14.
+//  Copyright (c) 2014 Chariot Instruments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CICBluetoothHandler : NSObject
+
+@end
---
+++ ChariotGauge/CICBluetoothHandler.m
@@ -0,0 +1,13 @@
+//
+//  CICBluetoothHandler.m
+//  ChariotGauge
+//
+//  Created by Mike on 1/12/14.
+//  Copyright (c) 2014 Chariot Instruments. All rights reserved.
+//
+
+#import "CICBluetoothHandler.h"
+
+@implementation CICBluetoothHandler
+
+@end
