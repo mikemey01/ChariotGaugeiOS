41bccd64320e20135b67e9985b402471ef7cf459
mikemey01 <mikemey01@gmail.com>
2014-01-24 18:37:24 -0700

    added gauge calc handler class

---
+++ ChariotGauge/CICCalculateData.h
@@ -0,0 +1,15 @@
+//
+//  CICCalculateData.h
+//  ChariotGauge
+//
+//  Created by Mike on 1/24/14.
+//  Copyright (c) 2014 Chariot Instruments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CICCalculateData : NSObject
+
+-(float) calculateBoost:(NSInteger)val;
+
+@end
---
+++ ChariotGauge/CICCalculateData.m
@@ -0,0 +1,36 @@
+//
+//  CICCalculateData.m
+//  ChariotGauge
+//
+//  Created by Mike on 1/24/14.
+//  Copyright (c) 2014 Chariot Instruments. All rights reserved.
+//
+
+#import "CICCalculateData.h"
+
+#define ATMOSPHERIC 101.325
+#define PSI_TO_INHG 2.03625437
+#define KPA_TO_PSI  0.14503773773020923
+#define KPA_TO_INHG 0.295299830714
+
+@implementation CICCalculateData
+
+-(float) calculateBoost:(NSInteger)val
+{
+    
+    float vOut;
+    float kpa=0;
+    float psi=0;
+    
+    vOut = (val*5.00)/1024;
+    kpa = ((vOut/5.00)+.04)/.004;
+    psi = (kpa - ATMOSPHERIC) * KPA_TO_PSI;
+    
+    if(psi < 0){
+        psi = psi * PSI_TO_INHG;
+    }
+    
+    return psi;
+}
+
+@end
