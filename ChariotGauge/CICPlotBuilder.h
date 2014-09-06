81fa5b610d6f1488c93150fc401a148eaa767caf
mikemey01 <mikemey01@gmail.com>
2014-09-06 16:39:42 -0600

    adding plot builder class

---
+++ ChariotGauge/CICPlotBuilder.h
@@ -0,0 +1,13 @@
+//
+//  CICPlotBuilder.h
+//  ChariotGauge
+//
+//  Created by Mike on 9/6/14.
+//  Copyright (c) 2014 Chariot Instruments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CICPlotBuilder : NSObject
+
+@end
---
+++ ChariotGauge/CICPlotBuilder.m
@@ -0,0 +1,13 @@
+//
+//  CICPlotBuilder.m
+//  ChariotGauge
+//
+//  Created by Mike on 9/6/14.
+//  Copyright (c) 2014 Chariot Instruments. All rights reserved.
+//
+
+#import "CICPlotBuilder.h"
+
+@implementation CICPlotBuilder
+
+@end
