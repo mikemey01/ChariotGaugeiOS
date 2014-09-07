c1ae5003394e47fbdf190c8f88220e4f6ab0e875
mikemey01 <mikemey01@gmail.com>
2014-09-07 17:09:38 -0600

    added dual chart view controller

---
+++ ChariotGauge/CICDualChartViewController.h
@@ -0,0 +1,28 @@
+//
+//  CICDualChartViewController.h
+//  ChariotGauge
+//
+//  Created by Mike on 9/7/14.
+//  Copyright (c) 2014 Chariot Instruments. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CICChartBuilder.h"
+#import "CICBluetoothHandler.h"
+#import "CICHomeScreenViewController.h"
+#import "CorePlot-CocoaTouch.h"
+
+@class CICChartBuilder, CICChartViewController;
+
+@interface CICDualChartViewController : UIViewController <BluetoothDelegate>{
+    CICChartBuilder *chartView;
+    CICBluetoothHandler *bluetooth;
+    CICPlotBuilder *_localPlotBuilderOne;
+    NSTimer *dataTimer;
+}
+
+@property GaugeType gaugeType;
+@property (nonatomic, retain) IBOutlet CICChartBuilder *chartView;
+@property (nonatomic, retain) CICBluetoothHandler *bluetooth;
+
+@end
